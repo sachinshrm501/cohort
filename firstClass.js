d2178960019533d2f3fe9b54bb5d90aab650c07b
Sachin Sharma <sachinsharma@ip-192-168-1-213.eu-west-1.compute.internal>
2025-02-27 11:02:48 +0530

    commit

---
+++ firstClass.js
@@ -0,0 +1,16 @@
+//NodeJs is like a company manager 
+//Sales person like as express (handle the user request)
+//WorkShop as a database 
+// Initially JS run only in browsers
+//NodeJs and burn both are the same but the difference is that nodejs is a runtime environment and burn is a browser runtime environment
+//NodeJs is a runtime environment that allows us to run JS code on the server
+//() // parenthesis
+function printChain(){
+    console.log('Hello World');
+}
+printChain();
+//`` // backtick template string
+function printAmout(amount){
+    console.log(`Amount: ${amount}`);
+}
+printAmout(100);
